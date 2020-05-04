350154c794691672d02162168cea2a59276b1cc4
jrhadams <j.rh.adams@outlook.com>
2020-05-04 20:25:39 +0200

    Initialisation of the project with a proof of concept example.

---
+++ PQRShiny_Proof_of_Concept/app.R
@@ -0,0 +1,55 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Beta Distribution Proof of Concept"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("shape1",
+                        "a :",
+                        min = 1,
+                        max = 20,
+                        value = 3),
+            sliderInput("shape2",
+                        "b :",
+                        min = 1,
+                        max = 20,
+                        value = 2)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        #Get your distributon
+#        x<- sort(dbeta(shape1=input$shape1,shape2=input$shape2,x=1:10000))
+        x<- sort(rbeta(10000,input$shape1,input$shape2))
+        y<- dbeta(x,shape1=input$shape1,shape2=input$shape2)
+        library(ggplot2)
+        ggplot(data.frame(x,y),aes(x,y))+geom_line(color='red',alpha=0.8,size=1.5)+
+            theme_minimal()+xlab('Quantile')+ylab('Density')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PQRShiny
 An attempt to enable PQRS within an R shiny interface
+
+## Summary
+I haven't been able to find any modern PQRS apps that are still functional. I'm planning to construct a Shiny version here.
+
+So far I only have a proof of concept for a beta distribution. The next step would be to highlight areas under the curve to visualise how hypothesis testing works.
